91cbc3592ed8b828ab4f3210b17324825679bd20
Saadkhaan12 <saadkkhan321@gmail.com>
2025-02-02 15:54:39 +0500

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ import dotenv from 'dotenv';
 dotenv.config({ path: 'config.env' });
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || '',
-  SUDO: process.env.SUDO || '',
-  BOT_INFO: process.env.BOT_INFO || 'αѕтяσχ11;χѕтяσ м∂',
+  SESSION_ID: process.env.SESSION_ID || 'XSTRO_02_80_44',
+  SUDO: process.env.SUDO || '923176991036',
+  BOT_INFO: process.env.BOT_INFO || 'SWATI-TEACH',
   WARN_COUNT: process.env.WARN_COUNT || 3,
   TIME_ZONE: process.env.TIME_ZONE || 'Africa/Lagos',
   VERSION: '1.4.0',
